3ac8872976284c24eb6014d09061a48afefe4720
qudeo <korapadug@gmail.com>
2023-11-29 15:39:19 +0900

    first commit

---
+++ src/a/first.java
@@ -0,0 +1,16 @@
+// 현재 클래스(자바파일)가 소속된 패키지의 이름
+package a;
+
+// 현재 클래스의 이름
+public class first { // first class 블록의 시작점
+
+	// main 메서드(프로그램의 시작 지점)
+	public static void main(String[] args) { // main 메서드 블록의 시작
+		// TODO Auto-generated method stub
+		// 실행할 내용이 작성되는 공간
+		
+		System.out.println("자바야 앞으로 친하게 지내자");
+
+	} // main 메서드 블록의 끝
+
+}// first class 블록의 끝
